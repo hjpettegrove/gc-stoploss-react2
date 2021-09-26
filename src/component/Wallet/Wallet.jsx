3ef1a840d69892254b9804cf590e6219aed5d843
hjpettegrove <hjpettegrove@gmail.com>
2021-09-26 14:40:41 -0700

    Building wallet

--- src/component/Wallet/Wallet.jsx
+++ src/component/Wallet/Wallet.jsx
@@ -1,27 +1,47 @@
 import { useEffect, useState } from "react";
-
-
+import * as WalletServices from '../../services/WalletService.js';
+import '../../styles/Wallet.css';
+/**
 const walletItem = ({id, symbol, icon, amount}) => {
     return ( 
 
      );
 }
- 
-export default walletItem;
+*/
 
+
+const formatToCash = (amount) => {
+    return parseFloat(amount).toFixed(2)
+}
 
 const Wallet = ({account}) => {
 
     const [walletBalance, setWalletBalance] = useState(null)
 
     useEffect(() => {
+        let loaded = true
         //  make fetch api call and
-        
+        let api_url = `http://localhost:9013/api/wallet/balance/1/${account}`;
+        fetch(api_url)
+            .then(resp => resp.json())
+            .then(data => {
+                if(loaded) {
+                    setWalletBalance(data[0])
+                    console.log(data[0])
+                }
+            })        
+        return () => loaded = false;
     }, [account] )
 
-    return ( 
+    return walletBalance ? ( 
+        <div className="wallet-balance">
+            <div className="wallet-item">Tokens balance: {formatToCash(walletBalance.balances.token_balance)}</div>
+            <div className="wallet-item">Pairs balance: {formatToCash(walletBalance.balances.pair_balance)}</div>
+            <div className="wallet-item">Total balance: {formatToCash(walletBalance.balances.total_balance)}</div>
+        </div>
+    ): (
         <div>
-
+            <p>...loading wallet</p>
         </div>
     );
 }
