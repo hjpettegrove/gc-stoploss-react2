68f60ec56ca743bb003209c570a637a88eead237
hjpettegrove <hjpettegrove@gmail.com>
2021-09-26 14:40:20 -0700

    created service for API access

--- src/services/WalletService.js
+++ src/services/WalletService.js
@@ -1,3 +1,8 @@
+const API_URL_BASE = 'http://localhost:9013'
+
+
 export function getWalletBalance(account, network = 1) {
-    
+    const api_wallet_balance_url = `${API_URL_BASE}/api/wallet/balance/${network}/${account}`
+    return fetch(api_wallet_balance_url)
+            .then(data => data.json())
 }
