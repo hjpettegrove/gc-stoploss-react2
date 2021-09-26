8c349257d6f7050f5d15b3119993ba6a50fc2d76
hjpettegrove <hjpettegrove@gmail.com>
2021-09-26 14:39:31 -0700

    Added wallet, tried to fix disconnect issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { useState } from 'react/cjs/react.development';
 import './styles/App.css';
 import MenuBar from './component/MenuBar/MenuBar.jsx';
+import Wallet from './component/Wallet/Wallet.jsx'
 
 function App() {
 
   const [account, setAccount] = useState(null);
 
   const handleWalletConnect = (account) => {
-    console.log(account)
     setAccount(account)
+  }
+
+  const handleWalletDisconnect = (cb = null) => {
+    setAccount(null);
+    return cb;
   }
 
   return (
     <div className="App">
-      <MenuBar handleWalletConnect={handleWalletConnect} />
+      {/*FIXME - Need to have some kind custom diconnect so as to also update the account value to null*/}
+      <MenuBar handleWalletConnect={handleWalletConnect} handleWalletDisconnect={handleWalletDisconnect}/>
       <div className="content container-md">
         
         <p>Account number is: {account ? account : 'wallet not connected'}</p>
+
+        <Wallet account={account} />
 
       </div>
     </div>
