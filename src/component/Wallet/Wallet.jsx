789356bcd168afb0a4767c0e0c49554c2543fc2a
hjpettegrove <hjpettegrove@gmail.com>
2021-09-26 15:48:20 -0700

    Token balances display completed

--- src/component/Wallet/Wallet.jsx
+++ src/component/Wallet/Wallet.jsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react";
 import * as WalletServices from '../../services/WalletService.js';
 import '../../styles/Wallet.css';
-/**
-const walletItem = ({id, symbol, icon, amount}) => {
-    return ( 
+import { toSixDecimals, toCash } from "../../helpers/formatters.js";
 
-     );
+
+const WalletItem = ({token}) => {
+    return ( 
+        <div className="wallet-token-item">
+            <img src={token.icon} alt="token image"/>
+            <span>{token.symbol}</span>
+            <span className="token_value">{toSixDecimals(token.amount)}</span>
+            <span>${toCash(token.value_usd)}</span>
+        </div>
+    );
 }
-*/
+
 
 
 const formatToCash = (amount) => {
@@ -17,6 +24,7 @@ const formatToCash = (amount) => {
 const Wallet = ({account}) => {
 
     const [walletBalance, setWalletBalance] = useState(null)
+    const [tokenPrices, setTokenPrices] = useState(null)
 
     useEffect(() => {
         let loaded = true
@@ -34,12 +42,23 @@ const Wallet = ({account}) => {
     }, [account] )
 
     return walletBalance ? ( 
-        <div className="wallet-balance">
-            <div className="wallet-item">Tokens balance: {formatToCash(walletBalance.balances.token_balance)}</div>
-            <div className="wallet-item">Pairs balance: {formatToCash(walletBalance.balances.pair_balance)}</div>
-            <div className="wallet-item">Total balance: {formatToCash(walletBalance.balances.total_balance)}</div>
+        <div className="wallet">
+            <div className="wallet-balance">
+                <div className="wallet-item">Tokens balance: {formatToCash(walletBalance.balances.token_balance)}</div>
+                <div className="wallet-item">Pairs balance: {formatToCash(walletBalance.balances.pair_balance)}</div>
+                <div className="wallet-item">Total balance: {formatToCash(walletBalance.balances.total_balance)}</div>
+            </div>
+            <div className="token-balance">
+
+                {walletBalance.assets.tokens.map((token) => {
+                    return <WalletItem token={token}/>
+                })}
+            </div>
+            <div className="pairs-balance">
+                
+            </div>
         </div>
-    ): (
+    ) : (
         <div>
             <p>...loading wallet</p>
         </div>
